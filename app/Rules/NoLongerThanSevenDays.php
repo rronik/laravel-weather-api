f9366218a2b78eb0b2dd1475fe7d9d57788e56aa
Rron Nela <rrnela@Rrons-MBP.fritz.box>
2022-07-22 15:39:18 +0200

    Add custom validation rule

---
+++ app/Rules/NoLongerThanSevenDays.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Rules;
+
+use Carbon\Carbon;
+use Illuminate\Contracts\Validation\Rule;
+
+class NoLongerThanSevenDays implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param string $attribute
+     * @param mixed $value
+     * @return bool
+     */
+    public function passes($attribute, $value): bool
+    {
+        return Carbon::parse($value) < now()->addDays(7)->format('Y-m-d');
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message(): string
+    {
+        return 'Our API accepts up to 7 days from today\'s date.';
+    }
+}
